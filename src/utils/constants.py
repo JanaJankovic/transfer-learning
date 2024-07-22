cf8df9699d6c89a5acc10176b68e666910b47a3f
Jana Janković <jana.jankovic@student.um.si>
2024-07-22 17:33:36 +0200

    added datasets

---
+++ src/utils/constants.py
@@ -0,0 +1,8 @@
+india = { 'raw': '../data/raw/India-1994-2024.csv', 'processed': '../data/processed/India-1994-2024-Rice.csv' }
+indonesia = { 'raw': '../data/raw/Indonesia-2007-2023.csv', 'processed': '../data/processed/Indonesia-2007-2023-Rice.csv' }
+iran = { 'raw': '../data/raw/Iran-2012-2022.csv', 'processed': '../data/processed/Iran-2012-2022-Rice.csv' }
+senegal = { 'raw': '../data/raw/Senegal-2000-2023.csv', 'processed': '../data/processed/Senegal-2000-2023-Rice.csv' }
+ukraine = { 'raw': '../data/raw/Ukraine-2014-2023.csv', 'processed': '../data/processed/Ukraine-2014-2023-Rice.csv' }
+moldova = { 'raw': '../data/raw/Moldova-2008-2024.csv', 'processed': '../data/processed/Moldova-2008-2024-Rice.csv' }
+turkey = { 'raw': '../data/raw/Turkey-2013-2022.csv', 'processed': '../data/processed/Turkey-2013-2022-Rice.csv' }
+philipines = { 'raw': '../data/raw/Philipines-2000-2024.csv', 'processed': '../data/processed/Philipines-2000-2024-Rice.csv' }
---
+++ src/utils/preprocessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def sort_extract_price(data, commodity):
+    df = data.copy()
+    df = df[df['commodity'].str.contains(commodity, case=False, na=False)]
+    df['date'] = pd.to_datetime(df['date'])
+    df['usdprice'] = pd.to_numeric(df['usdprice'])
+    df = df.sort_values(by='date', ascending=True).reset_index(drop=True)
+    df_avg = df.groupby('date')['usdprice'].mean().round(3).reset_index()
+    return df_avg
