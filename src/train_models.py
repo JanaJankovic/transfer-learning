972492c9efe28a5942ee84f1c6dabd217c612665
Jana Janković <jana.jankovic@student.um.si>
2024-09-29 09:49:35 +0200

    add models

--- src/train_models.py
+++ src/train_models.py
@@ -8,17 +8,17 @@ if __name__ == '__main__':
     large_datasets = ['Bangladesh', 'India', 'Indonesia', 'Nepal', 'Pakistan', 'Philipines', 'Senegal']
     small_datasets = ['Argentina', 'Nigeria', 'Ukraine']
     
-    print('Training big models')
-    train.training_pipeline(large_datasets, commodity, c.get_large_model_results(), 200)
+    # print('Training big models')
+    # train.training_pipeline(large_datasets, commodity, c.get_large_model_results(), 200)
     
-    print('Training small models')
-    train.training_pipeline(small_datasets, commodity, c.get_small_model_results(), 200)
+    # print('Training small models')
+    # train.training_pipeline(small_datasets, commodity, c.get_small_model_results(), 200)
     
-    print('Transfer learning Argentina')
-    tl.transfer_learning_pipeline('Argentina', large_datasets, commodity, c.get_tl_model_results(), 200)
+    # print('Transfer learning Argentina')
+    # tl.transfer_learning_pipeline('Argentina', large_datasets, commodity, c.get_tl_model_results(), 200)
     
-    print('Transfer learning Nigeria')
-    tl.transfer_learning_pipeline('Nigeria', large_datasets, commodity, c.get_tl_model_results(), 200)
+    # print('Transfer learning Nigeria')
+    # tl.transfer_learning_pipeline('Nigeria', large_datasets, commodity, c.get_tl_model_results(), 200)
     
     print('Transfer learning Ukraine')
     tl.transfer_learning_pipeline('Ukraine', large_datasets, commodity, c.get_tl_model_results(), 200)
